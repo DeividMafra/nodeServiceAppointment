9608356e992fe585857e5d737409d2dce8336503
DeividMafra <deividmafra@aol.com>
2021-09-01 12:33:21 -0400

    cache and class-transform providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
 
@@ -12,15 +13,29 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUserRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequestDTO): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
-    users.forEach(user => {
-      user.password = '';
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `provider-list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      console.log('done!');
+
+      await this.cacheProvider.save(`provider-list:${user_id}`, users);
+    }
+
+    // users.forEach(user => {
+    //   user.password = '';
+    // });
     return users;
   }
 }
